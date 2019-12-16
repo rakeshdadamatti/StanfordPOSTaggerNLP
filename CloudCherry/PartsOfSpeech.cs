7880bc4b63dcb695d48a7e856eaf913ada077e48
rakesh dadamatti <rakeshdadamatti@gmail.com>
2019-12-16 17:09:49 +0530

    Update PartsOfSpeech.cs

--- CloudCherry/PartsOfSpeech.cs
+++ CloudCherry/PartsOfSpeech.cs
@@ -8,7 +8,7 @@ namespace CloudCherry
 {
    public class PartsOfSpeech
     {
-       private static PartsOfSpeech single_instance = null;
+       private static PartsOfSpeech singleInstance = null;
        private static dynamic tagger = null;
 
        private PartsOfSpeech()
@@ -18,9 +18,9 @@ namespace CloudCherry
 
        public static dynamic Initialize()
        {
-           if (single_instance == null)
+           if (singleInstance == null)
            {
-               single_instance = new PartsOfSpeech();
+               singleInstance = new PartsOfSpeech();
                var modelsDirectory = @"D:\projects\microsoft\CloudCherry\CloudCherry\stanford-postagger-full-2016-10-31\models";
                tagger = new MaxentTagger(modelsDirectory + @"\wsj-0-18-bidirectional-nodistsim.tagger");
            }
